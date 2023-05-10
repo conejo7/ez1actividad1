fcf9f88ad6c806953f8df00c0102a18a03eaa30e
conejo7 <erick_s5490@hotmail.com>
2023-05-10 00:27:21 -0500

    Arreglo navbar sin recargar pagina

--- src/productos/components/Navbar.jsx
+++ src/productos/components/Navbar.jsx
@@ -35,15 +35,15 @@ export const Navbar1 = () => {
             </nav>
             <Navbar className="navbar">
                 <Container>
-                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                    <Navbar.Brand as={Link} to="/home">Navbar</Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link href="/carne">CARNE NORMAL</Nav.Link>
-                        <Nav.Link href="/vip">CARNE VIP</Nav.Link>
-                        <Nav.Link href="/tienda">TIENDA</Nav.Link>
-                        <Nav.Link href="/compras">COMPRAR</Nav.Link>
-                        <Nav.Link href="/table">PRODUCTOS</Nav.Link>
-                        <Nav.Link href="/search">BUSCAR</Nav.Link>
-                        <Nav.Link href="/carrito">CARRITO DE COMPRAS {quantity}</Nav.Link>
+                        <Nav.Link as={Link} to="/carne">CARNE NORMAL</Nav.Link>
+                        <Nav.Link as={Link} to="/vip">CARNE VIP</Nav.Link>
+                        <Nav.Link as={Link} to="/tienda">TIENDA</Nav.Link>
+                        <Nav.Link as={Link} to="/compras">COMPRAR</Nav.Link>
+                        <Nav.Link as={Link} to="/table">PRODUCTOS</Nav.Link>
+                        <Nav.Link as={Link} to="/search">BUSCAR</Nav.Link>
+                        <Nav.Link as={Link} to="/carrito">CARRITO DE COMPRAS {quantity}</Nav.Link>
                     </Nav>
                     <button
                         className="nav-item nav-link btn"
--- src/productos/pages/CarneNormal.jsx
+++ src/productos/pages/CarneNormal.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {CarnesList} from "../components/CarnesList";
+import {ProductosContext} from "../components";
 
 export const CarneNormal = () => {
     return (
